83e5d9b2db6d1d2d8246ccdda900bfcb870622d1
Sami <samiullahch274@gmail.com>
2024-08-02 19:45:09 +0500

    add some changes

---
+++ inventory_date/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Product Fields",
+    "summary": "Product Fields",
+    "version": "17.0.0.0",
+    "maintainers": ["Sami Ullah"],
+    "category": "Inventory",
+    "website": "",
+    "author": "Sami Ullah",
+    "depends": ['stock'],
+    "data": [
+        # "views/inventory_date.xml",
+    ],
+    "application": False,
+    'auto_install': False,
+    "installable": True
+}
