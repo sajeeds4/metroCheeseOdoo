0bbf66fa12ab864e5ae90aabaa7ca948b57075cd
Gulam Mohammed Sajeed <sajeedalsaba@gmail.com>
2024-09-19 02:50:36 +0530

    [ADD] app aj_custom_quotation_report

---
+++ aj_custom_quotation_report/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Custom Quotation Report",
+    "summary": """Complete Customize report""",
+    "category": "Sales",
+    "version": "1.0.4",
+    "author": "Arun",
+    "license": "LGPL-3",
+    "depends": ['sale'],
+    "data": [
+        'report/report.xml',
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    'images': ['static/description/2.png'],
+}
